6e82fd499ef79dfe6043d4414d0fbdbd6855f44d
Lucas Parada <luc.science@gmail.com>
2025-05-25 22:16:59 +0100

    add a test_function in utils.py
    imported test_function from utils in main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from utils import test_function
+
+
 def main():
     values = [x ** 2 for x in range(10)]
     a = 1
@@ -7,6 +10,9 @@ def main():
     b = 1
     values3 = [x + b for x in values]
     print(values3)
+
+    # TEST NEW UTILS FILE
+    print(test_function())
 
 
 if __name__ == '__main__':
