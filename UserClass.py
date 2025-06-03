cceade329d56ca461e893660b72a1bb0f0ad5291
Lucas Parada <luc.science@gmail.com>
2025-06-03 12:00:58 +0100

    added parameter is_active to user class

--- UserClass.py
+++ UserClass.py
@@ -2,6 +2,7 @@ class User:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.is_active = True
 
     def __str__(self):
         return f'User: {self.name} - {self.age}'
