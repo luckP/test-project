8ba3ceec3159cab9b8b1933f7343928fa3cb0d4b
Lucas Parada <luc.science@gmail.com>
2025-06-03 10:03:06 +0100

    Change from clone2

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+# Change from clone2
+
+
+import asyncio
+import aiohttp
 from utils import test_function
 from test2 import test2
+import requests
 
 
 def main():
@@ -18,6 +24,41 @@ def main():
     test2()
 
     print("bla bla bla")
+
+    print(asyncio.run(fetchAsync()))
+
+    # function3()
+
+    function2()
+
+    fucntion1()
+
+
+def fucntion1():
+    my_list = [x for x in range(10)]
+
+    for x in my_list:
+        print(x)
+
+
+def function2():
+    print('asdf1')
+    print('asdf2')
+    print('asdf3')
+    print('asdf4')
+    print('asdf5')
+
+
+def function3() -> None:
+    url = 'https://en.wikipedia.org/wiki/Black_hole'
+    r = requests.get(url)
+    print(r.text)
+
+
+async def fetchAsync():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://en.wikipedia.org/wiki/Black_hole") as response:
+            return await response.text()
 
 
 if __name__ == '__main__':
